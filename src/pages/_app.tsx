ee9519afdfefbeeab6f484b2eb548c834542c34d
Ali Dehghani <dehghanialif@gmail.com>
2025-03-12 06:22:05 +0100

    fix: add proper TypeScript types to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />;
-// }
-
-
+import { AppProps } from 'next/app';
 import '../styles/globals.css'
 
-function MyApp({ Component, pagePros }) {
-  return <Component {...pagePros} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
